0671e7dfe69462b2a44a256f5c2a8ace0fe91cbf
olamideforreal <colamide2@gmail.com>
2022-04-21 16:35:26 +0100

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -34,4 +34,45 @@ typedef struct types
 	char* (*func)(va_list);
 } print;
 
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
+int _printf(const char *format, ...);
+
+/* get_print */
+int (*get_print(char s))(va_list, mods *);
+
+/* get_flags */
+int get_flags(char s, mods *f);
+
+/* print_alpha */
+int print_string(va_list l, mods *f);
+int print_char(va_list l, mods *f);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
+/* print_custom */
+int print_rot13(va_list l, mods *f);
+int print_rev(va_list l, mods *f);
+int print_bigS(va_list l, mods *f);
+_Bool isNonAlphaNumeric(char c);
+
+/* print_address */
+int print_address(va_list l, mods *f);
+
+/* print_percent */
+int print_percent(va_list l, mods *f);
+
+/* assertions */
+_Bool invalidInputs(const char *p);
+
+/* unit_tests */
+void tusBasicTester(void);
+void tusAdvancedTester(void);
+void tusSuperAdvanced(void);
+
 #endif
